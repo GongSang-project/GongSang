3c9ae7f057a90e10171e6673a4c290a02ec45bc3
yb2527 <yb2527@swu.ac.kr>
2025-08-20 01:54:10 +0900

    feat: 청년 후기 작성

--- review/urls.py
+++ review/urls.py
@@ -6,4 +6,5 @@ app_name = 'review'
 urlpatterns = [
     path('', views.review_list, name='review_list'),
     path('completed/', views.review_completed_list, name='review_completed_list'),
+    path('write/<int:request_id>/', views.review_write, name='review_write'),
 ]
