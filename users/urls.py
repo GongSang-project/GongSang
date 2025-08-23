ded8a8d32bd4dcc9ca2e7b1500a8f60e04cd4e82
yb2527 <yb2527@swu.ac.kr>
2025-08-23 12:40:08 +0900

    feat: 시니어 내 정보 보기 1차
    # Conflicts:
    #	review/migrations/0001_initial.py
    #	users/urls.py
    #	users/views.py

--- users/urls.py
+++ users/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
 
     path('senior/<int:senior_id>/room/<int:room_id>/', views.senior_profile, name='senior_profile'),
     path('profile/youth/<int:request_id>/', views.youth_profile, name='youth_profile'),
-    path('profile/youth/<int:youth_id>/reviews/', views.all_reviews_for_youth, name='all_reviews_for_youth'),
+    path('profile/youth/<int:youth_id>/reviews/', views.all_reviews, name='all_reviews'),
+
+    path('senior/mypage/', views.senior_info_view, name='senior_info'),
 ]
