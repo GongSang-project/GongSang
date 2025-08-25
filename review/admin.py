b577e03a45baeed5cf8517b19b5b80529506b1ed
yb2527 <yb2527@swu.ac.kr>
2025-08-25 13:23:27 +0900

    refactor: admin 페이지 수정

--- review/admin.py
+++ review/admin.py
@@ -1,21 +1,27 @@
 from django.contrib import admin
 from .models import Review
 
+
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('id', 'author', 'target_senior', 'target_youth', 'room', 'lived_period', 'satisfaction', 're_living_hope', 'is_anonymous', 'created_at')
-    list_filter = ('lived_period', 'satisfaction', 're_living_hope', 'is_anonymous', 'created_at')
-    search_fields = ('author__username', 'target_senior__username', 'target_youth__username', 'room__id')
-    raw_id_fields = ('author', 'target_senior', 'target_youth', 'room')
-    readonly_fields = ('created_at',)
-    fieldsets = (
-        ('후기 정보', {
-            'fields': ('author', 'target_senior', 'target_youth', 'room', 'lived_period', 'satisfaction', 're_living_hope', 'is_anonymous', 'contract_document')
-        }),
-        ('상세 후기', {
-            'fields': ('good_points', 'bad_points')
-        }),
-        ('메타 정보', {
-            'fields': ('created_at',)
-        }),
+    list_display = (
+        "author",
+        "target_senior",
+        "target_youth",
+        "room",
+        "satisfaction",
+        "re_living_hope",
     )
+    list_filter = (
+        "satisfaction",
+        "re_living_hope",
+        "is_anonymous",
+        "created_at",
+    )
+    search_fields = (
+        "author__username",
+        "target_senior__username",
+        "target_youth__username",
+        "room__id",
+    )
+    raw_id_fields = ("author", "target_senior", "target_youth", "room")
--- room/admin.py
+++ room/admin.py
@@ -1,27 +1,27 @@
 from django.contrib import admin
 from .models import Room
 
+
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
-    list_display = ('id', 'owner', 'deposit', 'rent_fee', 'address_province', 'address_city', 'can_short_term', 'created_at', 'is_land_register_verified')
-    list_filter = ('can_short_term', 'parking_available', 'pet_allowed', 'is_land_register_verified')
-    search_fields = ('owner__username', 'address_city', 'address_district')
-    readonly_fields = ('created_at', 'updated_at')
-    raw_id_fields = ('owner', 'contracted_youth')
-    fieldsets = (
-        ('기본 정보', {
-            'fields': ('owner', 'deposit', 'rent_fee', 'floor', 'area', 'utility_fee', 'can_short_term', 'available_date', 'toilet_count')
-        }),
-        ('주소 및 등기부등본', {
-            'fields': ('address_province', 'address_city', 'address_district', 'address_detailed', 'land_register_document', 'is_land_register_verified')
-        }),
-        ('계약 정보', {
-            'fields': ('contracted_youth',)
-        }),
-        ('시설 및 옵션', {
-            'fields': ('options', 'security_facilities', 'other_facilities', 'parking_available', 'pet_allowed', 'heating_type', 'nearest_subway')
-        }),
-        ('메타 정보', {
-            'fields': ('created_at', 'updated_at')
-        }),
+    list_display = (
+        "owner",
+        "address_city",
+        "rent_fee",
+        "can_short_term",
+        "parking_available",
+        "pet_allowed",
+        "created_at",
     )
+    list_filter = (
+        "can_short_term",
+        "parking_available",
+        "pet_allowed",
+        "heating_type",
+    )
+    search_fields = (
+        "owner__username",
+        "address_city",
+        "address_detailed",
+    )
+    raw_id_fields = ("owner", "contracted_youth")
